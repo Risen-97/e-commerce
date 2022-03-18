120c8bf7be0a3182ebea476ec04d01274b9670bc
Risen-97 <m.fareeed1997@gmail.com>
2022-03-19 00:54:33 +0300

    added swiper lib

--- src/components/ui/HomeSlider.js
+++ src/components/ui/HomeSlider.js
@@ -1,74 +1,31 @@
-import { useState, useEffect, useRef } from "react";
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { hero } from "../../data";
+import { Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-let count = 0;
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 const HomeSlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const slideRef = useRef();
-
-  const removeAnimation = () => {
-    slideRef.current.classList.remove("fade-anim");
-  };
-
-  useEffect(() => {
-    slideRef.current.addEventListener("animationend", removeAnimation);
-    // startSlider();
-    return () =>
-      slideRef.current.removeEventListener("animationend", removeAnimation);
-  }, []);
-
-  const startSlider = () => {
-    setInterval(() => {
-      nextImageHandler();
-    }, 5000);
-  };
-  const nextImageHandler = () => {
-    count = (count + 1) % hero.length;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
-  };
-  const prevImageHandler = () => {
-    count = (count + hero.length - 1) % hero.length;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
-  };
-
   return (
-    <div ref={slideRef} className="w-full select-none relative ">
-      <div className="absolute tex-3xl md:text-4xl z-40 top-1/2 transform -translate-y-1/2 w-full px-3 text-white flex justify-between items-center">
-        <button
-          className="bg-white text-slate-800 rounded-full p-2"
-          onClick={prevImageHandler}
-        >
-          <AiOutlineArrowLeft />
-        </button>
-        <button
-          className="bg-white text-slate-800 rounded-full p-2"
-          onClick={nextImageHandler}
-        >
-          <AiOutlineArrowRight />
-        </button>
-      </div>
-
-      <div className="hidden absolute z-20 bottom-6 left-2/4 transform -translate-x-1/2 md:flex gap-8">
-        {hero.map((circle, i) => {
-          return (
-            <button
-              key={i}
-              className={`w-6 h-6 rounded-full  ${
-                i === currentIndex ? "bg-slate-400" : "bg-white"
-              }`}
-            ></button>
-          );
-        })}
-      </div>
-      <div className="w-full h-full z-10 bg-black absolute opacity-50"></div>
-      <div className="aspect-w-16 relative aspect-h-9 w-full">
-        <img className="w-full " src={hero[currentIndex].image} alt="" />
-      </div>
-    </div>
+    <Swiper
+      modules={[Pagination, Navigation]}
+      spaceBetween={0}
+      loop={true}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      navigation
+    >
+      {hero.map((cover) => {
+        return (
+          <SwiperSlide key={cover.id} className="w-full  relative">
+            <div className="w-full h-full z-10 bg-black absolute opacity-50"></div>
+            <img src={cover.image} className="w-full h-full" />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
